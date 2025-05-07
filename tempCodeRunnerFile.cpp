10b3cd6ce86b99ec2c9f3a9c71e370f1c89aaa95
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2025-05-07 15:38:09 +0700

    memasukan banyk elemen

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void input()
+{
+
+while (true)
+{
+
+   cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+   cin >> nPanjang;
+    if (nPanjang <= 10)
+    {
+
+        break;
+    }
+    else
+    {
+        cout<<"\n[!] Jumlan elemen tidak boleh lebih dari 10. 51lakan coba lagi.\n";
+
+    }
+}
